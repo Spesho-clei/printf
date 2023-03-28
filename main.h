fbd1d0ff7ce67f1bdd18bba3cbd3af36eda03c93
decanton <owuordickens@gmail.com>
2023-03-28 03:34:31 -0700

    printf project

--- main.h
+++ main.h
@@ -72,5 +72,6 @@ int print_address(va_list l, flags_t *f);
 
 /* print_percent */
 int print_percent(va_list l, flags_t *f);
-
+/* get preciosn */
+int get_precision(const char *format, int *i, va_list list);
 #endif
