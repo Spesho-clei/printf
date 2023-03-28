6c5262d7b0b883fac7097a54f1fa9998661a405e
Spesho-clei <jepkoechnancy56@gmail.com>
2023-03-27 23:36:41 -0700

    printf project

--- main.h
+++ main.h
@@ -1,35 +1,44 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
-#include <stdarg.h>
-#include <stdlib.h>
-#include <unistd.h>
-/**
- * struct print - struct for print function
- * @conv_spec: conversion specifier.
- * @f: pointer to the printer function
- * Return: none
- */
-typedef struct print
-{
-	char *conv_spec;
-	int (*f)(va_list, char *);
-} print_t;
+#ifndef MAIN_H
+#define MAIN_H
 
-int (*switch_funct(const char *s, int *pos))(va_list, char *);
-int print_char(va_list args, char *buffer);
-int print_str(va_list args, char *buffer);
-int print_int(va_list args, char *buffer);
-int print_unint(va_list args, char *buffer);
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+* struct convert - defines a structure for symbols and functions
+*
+* @sym: The operator
+* @f: The function associated
+*/
+struct convert
+{
+	char *sym;
+	int (*f)(va_list);
+};
+typedef struct convert conver_t;
+
+
+int parser(const char *format, conver_t f_list[], va_list arg_list);
 int _printf(const char *format, ...);
-int _print_buf(char *buf, int n);
-int print_bin(va_list args, char *buffer);
-int print_pointer(va_list, char *buffer);
-int print_low_hex(va_list, char *buffer);
-int print_upp_hex(va_list, char *buffer);
-int print_octal(va_list, char *buffer);
-int custom_str(va_list, char *buffer);
-int print_octal_zero(va_list args, char *buffer);
-int print_space_int(va_list args, char *buffer);
-int print_plus_int(va_list args, char *buffer);
+int _write_char(char);
+int print_char(va_list);
+int print_string(va_list);
+int print_percent(va_list);
+int print_integer(va_list);
+int print_number(va_list);
+int print_binary(va_list);
+int print_reversed(va_list arg);
+int rot13(va_list);
+int unsigned_integer(va_list);
+int print_octal(va_list list);
+int print_hex(va_list list);
+int print_heX(va_list list);
+
+unsigned int base_len(unsigned int, int);
+char *rev_string(char *);
+void write_base(char *str);
+char *_memcpy(char *dest, char *src, unsigned int n);
+int print_unsgined_number(unsigned int);
 
 #endif
